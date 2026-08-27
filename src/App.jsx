0801bf77d5f6798dd44ba5cc71451d1232278709
Mhd.Anggie Rinovka <45094075+MhdAnggieRinovka@users.noreply.github.com>
2026-08-27 17:36:35 +0700

    Fix Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import WorkDetailPage from "./pages/work-detail/WorkDetailPage";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<HomePage initialPage="home" />}
-      />
+      <Route path="/" element={<HomePage initialPage="home" />} />
 
-      <Route
-        path="/about_us"
-        element={<HomePage initialPage="about" />}
-      />
+      <Route path="/about_us" element={<HomePage initialPage="about" />} />
 
-      <Route
-        path="/works"
-        element={<HomePage initialPage="works" />}
-      />
+      <Route path="/works" element={<HomePage initialPage="works" />} />
 
-      <Route
-        path="/contacts"
-        element={<HomePage initialPage="contacts" />}
-      />
+      <Route path="/contacts" element={<HomePage initialPage="contacts" />} />
 
-      <Route
-        path="/work/:slug"
-        element={<WorkDetailPage />}
-      />
+      <Route path="/work/:slug" element={<WorkDetailPage />} />
     </Routes>
   );
 }
